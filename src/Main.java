bed69120f161ca7f1c7d83baeb0acf7867af5fb6
thiago <thiago.alves@ifce.edu.br>
2023-04-27 19:59:56 -0300

    adapta o programa para lidar com duas notas para cada aluno

--- src/Main.java
+++ src/Main.java
@@ -12,23 +12,27 @@ public class Main {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Digite a quantidade de alunos na turma: ");
 		int quantidadeAlunos = input.nextInt();
-		double[] notasAlunos = new double[quantidadeAlunos];
+		double[] notas1Alunos = new double[quantidadeAlunos];
+		double[] notas2Alunos = new double[quantidadeAlunos];
 
-		for (int i = 0; i < notasAlunos.length; i++){
-			System.out.printf("Digite a nota do %dº aluno: ", i+1);
-			notasAlunos[i] = input.nextDouble();
+		for (int i = 0; i < notas1Alunos.length; i++){
+			System.out.printf("Digite a nota 1 do %dº aluno: ", i+1);
+			notas1Alunos[i] = input.nextDouble();
+
+			System.out.printf("Digite a nota 2 do %dº aluno: ", i+1);
+			notas2Alunos[i] = input.nextDouble();
 		}
 
 //		for (int i = 0; i < notasAlunos.length; i++){
 //			System.out.printf("Nota do %dº aluno: %.2f%n", i+1, notasAlunos[i]);
 //		}
 
-		double somaNotas = 0;
-		for (int i = 0; i < notasAlunos.length; i++){
-			somaNotas += notasAlunos[i];
+		double somaMedias = 0;
+		for (int i = 0; i < notas1Alunos.length; i++){
+			somaMedias += (notas1Alunos[i] + notas2Alunos[i])/2.0;
 		}
 
-		double mediaTurma = somaNotas/quantidadeAlunos;
+		double mediaTurma = somaMedias/quantidadeAlunos;
 		System.out.printf("Média da turma: %.2f%n", mediaTurma);
 
 		int contador = 0;
